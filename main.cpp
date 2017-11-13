53d47051fc5e7ea7403e4ca8638f4d9181513363
Joseph Kleinhenz <kleinhenz.joseph@gmail.com>
2017-11-12 19:06:30 -0500

    implement get

--- main.cpp
+++ main.cpp
@@ -1,27 +1,34 @@
 #include <array>
+#include <tuple>
 #include <boost/preprocessor.hpp>
 #include <iostream>
 #include <string>
-
-template<typename ... Types> struct type_list {};
 
 template <typename T> struct parameter {
   parameter(T val, std::string name, std::string help)
       : val(val), name(name), help(help) {}
   operator T() const { return val; }
   T val;
-  std::string name;
-  std::string help;
+  const std::string name;
+  const std::string help;
 };
 
 #define DEFINE_PARAMETERS_STRUCT(name, ...)                                    \
   DEFINE_PARAMETERS_STRUCT_IMPL(name, BOOST_PP_VARIADIC_TO_SEQ(__VA_ARGS__))
 
-// TODO should Parameters type be templated on the contained types???
 #define DEFINE_PARAMETERS_STRUCT_IMPL(name, members_seq)                       \
   struct name {                                                                \
+    static constexpr size_t size = BOOST_PP_SEQ_SIZE(members_seq);             \
     GENERATE_MEMBERS(members_seq)                                              \
-  }
+  };                                                                           \
+  template <size_t I> auto &get(name &x);                                      \
+  GENERATE_GETS(name, members_seq);
+
+#define PARAM_TYPE_SEQ(members_seq) \
+  BOOST_PP_SEQ_TRANSFORM(PARAM_TYPE_SEQ_OP,_,members_seq)
+
+#define PARAM_TYPE_SEQ_OP(s, data, member) \
+  PARAM_TYPE(member)
 
 #define PARAM_TYPE(member) BOOST_PP_TUPLE_ELEM(0, member)
 #define PARAM_NAME(member) BOOST_PP_TUPLE_ELEM(1, member)
@@ -37,28 +44,19 @@ template <typename T> struct parameter {
   parameter<PARAM_TYPE(member)> PARAM_NAME(member) = {                         \
       PARAM_DEFAULT(member), PARAM_NAME_STR(member), PARAM_HELP(member)};
 
-/*
-#define GENERATE_GETS(members_seq) \
-  BOOST_PP_SEQ_FOR_EACH_I(GENERATE_GET, , members_seq)
+#define GENERATE_GETS(name, members_seq)                                       \
+  BOOST_PP_SEQ_FOR_EACH_I(GENERATE_GET, name, members_seq)
 
-#define GENERATE_GET(r, data, i, member) \
-  template<size_t i> get(
-  */
-
+#define GENERATE_GET(r, name, i, member)                                       \
+  template <> auto &get<i>(name & x) { return x.PARAM_NAME(member); }
 
 DEFINE_PARAMETERS_STRUCT(Parameters,
                          (int, i, 0, "i param"),
-                         (int, j, 1, "j param"));
-
-/*
-template<typename T, size_t I> T get(const Parameters& p) {}
-template<> int get<0>(const Parameters &p){return p.i;}
-template<> int get<1>(const Parameters &p){return p.j;}
-*/
+                         (int, j, 1, "j param"))
 
 int main(int argc, char *argv[]) {
   Parameters p;
 
-  std::cout << "i = " << p.i << "\n";
-  std::cout << "j = " << p.j << "\n";
+  std::cout << "i = " << get<0>(p) << "\n";
+  std::cout << "j = " << get<1>(p) << "\n";
 }
